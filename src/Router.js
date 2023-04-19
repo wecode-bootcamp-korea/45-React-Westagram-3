84514c04e99d5f0401072fb3a0d4f6e582f3ffd1
min2oyo <min2u@outlook.com>
2023-04-19 15:36:25 +0900

    convention setting

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Router = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Router from './Router';
+import ReactDOM from "react-dom/client";
+import Router from "./Router";
 import "./styles/reset.scss";
 import "./styles/variables.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router />
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Router />);
