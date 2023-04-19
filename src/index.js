fec8620358ff704ef8e495955e1f4a13165c3931
min2oyo <min2u@outlook.com>
2023-04-19 17:45:54 +0900

    Fix: 초기 셋팅 피드백 1차 적용

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
 import "./styles/reset.scss";
+import "./styles/common.scss";
 import "./styles/variables.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
