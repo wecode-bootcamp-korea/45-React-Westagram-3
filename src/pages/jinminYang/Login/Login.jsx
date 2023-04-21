79a5ecc094594215d75c6e852b082851e45c5bab
min2oyo <min2u@outlook.com>
2023-04-21 09:40:59 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jinminYang/Login/Login.jsx
+++ src/pages/jinminYang/Login/Login.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Login.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const [id, setId] = useState(null);
+  const [pw, setPw] = useState(null);
+
+  const saveUserId = e => {
+    setId(e.target.value);
+  };
+
+  const saveUserPw = e => {
+    setPw(e.target.value);
+  };
+
   return (
     <div className="login">
       <div className="compNameDiv">
@@ -13,8 +25,14 @@ const Login = () => {
           className="id"
           type="text"
           placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={e => saveUserId(e)}
         />
-        <input className="pw" type="password" placeholder="비밀번호" />
+        <input
+          className="pw"
+          type="password"
+          placeholder="비밀번호"
+          onChange={e => saveUserPw(e)}
+        />
         <button className="loginButton" type="submit">
           로그인
         </button>
