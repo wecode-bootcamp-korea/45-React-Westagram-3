c92fc989041a4258477ff3587be8740408171d62
김준섭 <junsobi@gmail.com>
2023-04-26 15:42:38 +0900

    상수데이터부분 새로운 파일로 빼서 관리

--- src/pages/junsobKim/Main/RightSection_kim.js
+++ src/pages/junsobKim/Main/RightSection_kim.js
@@ -1,4 +1,5 @@
 import React from "react";
+import COMPANYDETAIL from "./COMPANYDETAIL";
 import "./RightSection_kim.scss";
 
 export const RightSection_kim = () => {
@@ -147,19 +148,3 @@ const CompanyDetail = () => {
     </div>
   );
 };
-
-const COMPANYDETAIL = [
-  { id: 1, name: "Instagram", address: "" },
-  { id: 2, name: "정보", address: "" },
-  { id: 3, name: "지원", address: "" },
-  { id: 4, name: "홍보 센터", address: "" },
-  { id: 5, name: "API", address: "" },
-  { id: 6, name: "채용 정보", address: "" },
-  { id: 7, name: "개인정보처리방침", address: "" },
-  { id: 8, name: "약관", address: "" },
-  { id: 9, name: "디렉터리", address: "" },
-  { id: 10, name: "프로필", address: "" },
-  { id: 11, name: "해시태그", address: "" },
-  { id: 12, name: "언어", address: "" },
-  { id: 13, name: "© 2019 INSTAGRAM", address: "" },
-];
