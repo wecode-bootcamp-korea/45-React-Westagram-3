a0bb9ef9ffd8af0f311460bb739152f012050b2a
김준섭 <junsobi@gmail.com>
2023-04-26 15:36:46 +0900

    right-section에서(map을 활용한) 상수데이터 활용

--- src/pages/junsobKim/Main/RightSection_kim.js
+++ src/pages/junsobKim/Main/RightSection_kim.js
@@ -136,12 +136,30 @@ const RecommendFollow = () => {
 const CompanyDetail = () => {
   return (
     <div className="companyD">
-      Instagram 정보 • 지원 • 홍보 센터 • API
-      <br />
-      채용 정보 • 개인정보처리방침 • 약관 •
-      <br />
-      디렉터리 • 프로필 • 해시태그 • 언어
-      <br />© 2019 INSTAGRAM
+      {COMPANYDETAIL.map(({ id, name, address }, index) => (
+        <React.Fragment key={id}>
+          <a className="details" href={address}>
+            {name}
+          </a>
+          {index !== COMPANYDETAIL.length - 1 && <span>•</span>}
+        </React.Fragment>
+      ))}
     </div>
   );
 };
+
+const COMPANYDETAIL = [
+  { id: 1, name: "Instagram", address: "" },
+  { id: 2, name: "정보", address: "" },
+  { id: 3, name: "지원", address: "" },
+  { id: 4, name: "홍보 센터", address: "" },
+  { id: 5, name: "API", address: "" },
+  { id: 6, name: "채용 정보", address: "" },
+  { id: 7, name: "개인정보처리방침", address: "" },
+  { id: 8, name: "약관", address: "" },
+  { id: 9, name: "디렉터리", address: "" },
+  { id: 10, name: "프로필", address: "" },
+  { id: 11, name: "해시태그", address: "" },
+  { id: 12, name: "언어", address: "" },
+  { id: 13, name: "© 2019 INSTAGRAM", address: "" },
+];
